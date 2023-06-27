d9c90e37b311c004849cab33c1856bdd9cdc6747
Venture_Ronin <venture.ronin@gmail.com>
2023-06-27 21:20:02 +0300

    1

---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+variable "def_name" {
+	default = "mondysput"
+}
+
+variable "region_prj" {
+	default = "us-central1"
+}
+
+variable "zone_prj" {
+	default = "us-central1-a"
+}
+
+variable "sa_name" {
+	default = "sa-tfstate"
+}
+
+variable "sa_account" {
+	default = ".iam.gserviceaccount.com"
+}
+
+variable "domain" {
+	default = "ventureronindomain.pp.ua"
+}
+
+variable "local_ip_range" {
+	default = "10.100.0.0/28"
+}
+
+variable "gke_ip_range" {
+	default = "10.200.0.0/19"
